c4ab7ae5f58157dd1f8d4fc3a7f6b0825bc01a52
apurvmaurya <apurvmaurya5@gmail.com>
2023-12-13 02:58:29 +0530

    third

--- puppy.go
+++ puppy.go
@@ -2,7 +2,7 @@ package puppy
 
 import "github.com/gitApurv/dog"
 
-var PuppyName string = "Pilla"
+var PuppyName string = "Puppy"
 
 func Family(s string) string {
 	return dog.DogName + " son's name is " + PuppyName + " and his son's name is " + s
